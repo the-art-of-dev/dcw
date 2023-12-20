477d331a5ee5dad768cba1c13f8115ce1d46c8f3
Vukoman Stojanovic <vukomans96@yahoo.com>
2023-12-20 14:57:32 +0100

    Update k8s std maker

--- dcw/std.py
+++ dcw/std.py
@@ -65,7 +65,7 @@ class K8SDeploymentMaker(DCWDeploymentMaker):
 
         service = depl_spec.services[name]
         if 'dcw.kompose.service.loadbalancerip' in service['labels']:
-            k8s_svc['annotations']['service.beta.kubernetes.io/azure-load-balancer-internal'] = 'true'
+            k8s_svc['metadata']['annotations']['service.beta.kubernetes.io/azure-load-balancer-internal'] = 'true'
             k8s_svc['spec']['loadBalancerIP'] = service['labels']['dcw.kompose.service.loadbalancerip']
 
     def __enrich_k8s_kind(self, k8s_kind: dict, depl_spec: DCWDeploymentSpecification):
